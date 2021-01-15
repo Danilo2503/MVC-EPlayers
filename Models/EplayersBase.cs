08844a18d03660bf94ed191865dc55e1ad97462f
unknown <dannilorch@gmail.com>
2021-01-15 08:15:17 -0300

    Parte 5 finalizada

--- Models/EplayersBase.cs
+++ Models/EplayersBase.cs
@@ -7,12 +7,13 @@ namespace MVC_EPlayers.Models
     {
         public void CreateFileAndFolder(string path){
             string folder = path.Split("/")[0];
+            string file = path.Split("/")[1];
 
             if(!Directory.Exists(folder)){
                 Directory.CreateDirectory(folder);
             }
             if(!File.Exists(path)){
-                File.Create(path);   
+                File.Create(path).Close();   
             }
         }
         public List<string> ReadAllLinesCSV(string PATH){
@@ -36,23 +37,7 @@ namespace MVC_EPlayers.Models
                     output.Write(item + "\n");
                 }
             }
-        }
-        public void Update(Equipe equipe, string PATH){
-            List<string> linhas = ReadAllLinesCSV(PATH);
-            linhas.RemoveAll(x => x.Split(";")[0] == equipe.IdEquipe.ToString());
-            linhas.Add(Prepare(equipe));
-            RewriteCSV(PATH, linhas);
-        }
-
-        private string Prepare(Equipe equipe)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Delete(int id, string PATH){
-            List<string> linhas = ReadAllLinesCSV(PATH);
-            linhas.RemoveAll(x => x.Split(";")[0] == id.ToString());
-            RewriteCSV(PATH, linhas);
-        }        
+        }    
     }
-}
+}        
+        
